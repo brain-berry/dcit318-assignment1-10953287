3828463d7967542c5fd344a88e074858c81d1556
brain-berry <habenson001@st.ug.edu.gh>
2024-06-02 21:19:06 +0000

    Add Grade Calculator application

---
+++ GradeCalculator/Program.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace GradeCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a numerical grade (0-100): ");
+            int grade = int.Parse(Console.ReadLine());
+
+            string letterGrade;
+
+            if (grade >= 90)
+                letterGrade = "A";
+            else if (grade >= 80)
+                letterGrade = "B";
+            else if (grade >= 70)
+                letterGrade = "C";
+            else if (grade >= 60)
+                letterGrade = "D";
+            else
+                letterGrade = "F";
+
+            Console.WriteLine("The letter grade is: " + letterGrade);
+        }
+    }
+}
+
+
